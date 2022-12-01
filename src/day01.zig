47f6196fa756c2750fb2e710129ce74fb87d785a
JasperDeSutter <jasper.desutter@gmail.com>
2022-12-01 10:02:15 +0100

    day01: part 1

--- src/day01.zig
+++ src/day01.zig
@@ -5,13 +5,46 @@ pub fn main() anyerror!void {
     try runner.run(solve);
 }
 
-fn solve(_: std.mem.Allocator, _: []const u8) anyerror!void {
-    
+fn solve(_: std.mem.Allocator, input: []const u8) anyerror!void {
+    const mostTotal = try mostTotalCalories(input);
+    std.debug.print("most total: {}\n", .{mostTotal});
+}
+
+fn mostTotalCalories(input: []const u8) !u32 {
+    var iter = std.mem.split(u8, input, "\n");
+    var most: u32 = 0;
+    var current: u32 = 0;
+
+    while (iter.next()) |line| {
+        if (line.len == 0) {
+            if (current > most) most = current;
+            current = 0;
+            continue;
+        }
+        const amount = try std.fmt.parseInt(u32, line, 10);
+        current += amount;
+    }
+
+    return most;
 }
 
 test {
     const input =
-    \\
+        \\1000
+        \\2000
+        \\3000
+        \\
+        \\4000
+        \\
+        \\5000
+        \\6000
+        \\
+        \\7000
+        \\8000
+        \\9000
+        \\
+        \\10000
     ;
-    _ = input;
+
+    try std.testing.expectEqual(try mostTotalCalories(input), 24000);
 }
