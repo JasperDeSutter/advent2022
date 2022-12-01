f4dbae8abbb5b0f1923665092f652432738ea687
JasperDeSutter <jasper.desutter@gmail.com>
2022-12-01 10:45:57 +0100

    day01: part 2

--- src/day01.zig
+++ src/day01.zig
@@ -8,24 +8,52 @@ pub fn main() anyerror!void {
 fn solve(_: std.mem.Allocator, input: []const u8) anyerror!void {
     const mostTotal = try mostTotalCalories(input);
     std.debug.print("most total: {}\n", .{mostTotal});
+
+    const topThree = try topThreeCaloriesTotal(input);
+    std.debug.print("top three total: {}\n", .{topThree});
 }
 
 fn mostTotalCalories(input: []const u8) !u32 {
+    const buf = try mostTotalCaloriesTop(comptime 1, input);
+    return buf[0];
+}
+
+fn topThreeCaloriesTotal(input: []const u8) !u32 {
+    const buf = try mostTotalCaloriesTop(comptime 3, input);
+
+    var total: u32 = 0;
+    for (buf) |n| total += n;
+    return total;
+}
+
+fn lineEmpty(line: ?[]const u8) bool {
+    const l = line orelse return true;
+    return l.len == 0;
+}
+
+fn mostTotalCaloriesTop(comptime N: usize, input: []const u8) ![N]u32 {
     var iter = std.mem.split(u8, input, "\n");
-    var most: u32 = 0;
+
+    var buf = [_]u32{0} ** (N + 1);
     var current: u32 = 0;
 
-    while (iter.next()) |line| {
-        if (line.len == 0) {
-            if (current > most) most = current;
-            current = 0;
+    while (true) {
+        const line = iter.next();
+        if (!lineEmpty(line)) {
+            const amount = try std.fmt.parseInt(u32, line.?, 10);
+            current += amount;
             continue;
         }
-        const amount = try std.fmt.parseInt(u32, line, 10);
-        current += amount;
+        buf[N] = current;
+        std.sort.sort(u32, &buf, {}, comptime std.sort.desc(u32));
+
+        if (line == null) break;
+        current = 0;
     }
 
-    return most;
+    var result: [N]u32 = undefined;
+    std.mem.copy(u32, &result, buf[0..N]);
+    return result;
 }
 
 test {
@@ -47,4 +75,6 @@ test {
     ;
 
     try std.testing.expectEqual(try mostTotalCalories(input), 24000);
+
+    try std.testing.expectEqual(try topThreeCaloriesTotal(input), 45000);
 }
