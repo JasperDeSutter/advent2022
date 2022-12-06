05a27434fd3ea52914533fd4ef74c58f79e36850
JasperDeSutter <jasper.desutter@gmail.com>
2022-12-06 12:10:40 +0100

    day06: part 2

--- src/day06.zig
+++ src/day06.zig
@@ -9,6 +9,7 @@ fn solve(alloc: std.mem.Allocator, input: []const u8) anyerror!void {
     _ = alloc;
 
     std.debug.print("firstStartOfPacketMarker: {any}\n", .{firstStartOfPacketMarker(input)});
+    std.debug.print("firstStartOfMessageMarker: {any}\n", .{firstStartOfMessageMarker(input)});
 }
 
 fn RingBuffer(comptime T: type, comptime size: u8) type {
@@ -33,10 +34,18 @@ fn RingBuffer(comptime T: type, comptime size: u8) type {
 }
 
 fn firstStartOfPacketMarker(datastream: []const u8) usize {
-    var ringBuffer = RingBuffer(u8, 4){};
+    return impl(datastream, 4);
+}
+
+fn firstStartOfMessageMarker(datastream: []const u8) usize {
+    return impl(datastream, 14);
+}
+
+fn impl(datastream: []const u8, comptime size: u8) usize {
+    var ringBuffer = RingBuffer(u8, size){};
     for (datastream) |c, i| {
         ringBuffer.setNext(c);
-        if (i > 2 and ringBuffer.allDifferent()) {
+        if (i > (size - 2) and ringBuffer.allDifferent()) {
             return i + 1;
         }
     }
@@ -49,4 +58,10 @@ test {
     try std.testing.expectEqual(firstStartOfPacketMarker("nppdvjthqldpwncqszvftbrmjlhg"), 6);
     try std.testing.expectEqual(firstStartOfPacketMarker("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"), 10);
     try std.testing.expectEqual(firstStartOfPacketMarker("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"), 11);
+
+    try std.testing.expectEqual(firstStartOfMessageMarker("mjqjpqmgbljsphdztnvjfqwrcgsmlb"), 19);
+    try std.testing.expectEqual(firstStartOfMessageMarker("bvwbjplbgvbhsrlpgdmjqwftvncz"), 23);
+    try std.testing.expectEqual(firstStartOfMessageMarker("nppdvjthqldpwncqszvftbrmjlhg"), 23);
+    try std.testing.expectEqual(firstStartOfMessageMarker("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"), 29);
+    try std.testing.expectEqual(firstStartOfMessageMarker("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"), 26);
 }
