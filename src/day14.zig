ea78ef97078cefeb219e9ec3f07bbfb93395b2ac
JasperDeSutter <jasper.desutter@gmail.com>
2024-09-09 23:15:55 +0200

    day14: part 2

--- src/day14.zig
+++ src/day14.zig
@@ -1,79 +1,96 @@
 const std = @import("std");
 const runner = @import("runner.zig");
 
-pub const main = runner.run("13", solve);
+pub const main = runner.run("14", solve);
+
+const BitSet = [1000 * 256]bool;
+
+const Pos = struct {
+    x: u16,
+    y: u8,
+};
 
 fn solve(alloc: std.mem.Allocator, input: []const u8) anyerror![2]usize {
-    _ = alloc;
+    var max: u8 = 0;
 
-    var min: [2]u8 = .{ 255, 255 };
-    var max: [2]u8 = .{ 0, 0 };
+    var bitset: *BitSet = @ptrCast(try alloc.create(BitSet));
+    defer alloc.free(bitset);
+    @memset(bitset, false);
 
-    var bitset = std.bit_set.ArrayBitSet(usize, 256 * 256).initEmpty();
     {
         var lines = std.mem.tokenizeScalar(u8, input, '\n');
         while (lines.next()) |line| {
             var parts = std.mem.tokenizeSequence(u8, line, " -> ");
-            var prev: [2]u8 = .{ 0, 0 };
+            var prev = Pos{ .x = 0, .y = 0 };
             while (parts.next()) |part| {
-                var x: u8 = @intCast(try std.fmt.parseInt(u16, part[0..3], 10) - 400);
+                var x = try std.fmt.parseInt(u16, part[0..3], 10);
                 var y = try std.fmt.parseInt(u8, part[4..], 10);
 
                 const xP = x;
                 const yP = y;
 
-                if (x == prev[0]) {
-                    if (y < prev[1]) std.mem.swap(u8, &y, &prev[1]);
-                    for (prev[1]..(y + 1)) |i| {
-                        bitset.set(index(x, @intCast(i)));
+                if (x == prev.x) {
+                    if (y < prev.y) std.mem.swap(u8, &y, &prev.y);
+                    for (prev.y..(y + 1)) |i| {
+                        bitset[index(x, @intCast(i))] = true;
                     }
                 }
-                if (y == prev[1]) {
-                    if (x < prev[0]) std.mem.swap(u8, &x, &prev[0]);
-                    for (prev[0]..(x + 1)) |i| {
-                        bitset.set(index(@intCast(i), y));
+                if (y == prev.y) {
+                    if (x < prev.x) std.mem.swap(u16, &x, &prev.x);
+                    for (prev.x..(x + 1)) |i| {
+                        bitset[index(@intCast(i), y)] = true;
                     }
                 }
 
-                min = .{ @min(min[0], x), @min(min[1], y) };
-                max = .{ @max(max[0], x), @max(max[1], y) };
-
-                prev = .{ xP, yP };
+                if (yP > max) max = yP;
+                prev = Pos{ .x = xP, .y = yP };
             }
         }
     }
 
+    const count = simulate(max, max + 1, bitset);
+
+    const count2 = simulate(max + 2, max + 1, bitset);
+
+    return .{ count, count + count2 };
+}
+
+fn simulate(max: u8, floor: u8, bitset: *BitSet) usize {
     var count: usize = 0;
 
     outer: while (true) {
-        var pos: [2]u8 = .{ 100, 0 };
-        while (pos[1] <= max[1]) {
-            if (!bitset.isSet(index(pos[0], pos[1] + 1))) {
-                pos[1] += 1;
-                continue;
+        var pos = Pos{ .x = 500, .y = 0 };
+        while (pos.y < max) {
+            if (pos.y < floor) {
+                if (!bitset[index(pos.x, pos.y + 1)]) {
+                    pos.y += 1;
+                    continue;
+                }
+                if (!bitset[index(pos.x - 1, pos.y + 1)]) {
+                    pos.x -= 1;
+                    pos.y += 1;
+                    continue;
+                }
+
+                if (!bitset[index(pos.x + 1, pos.y + 1)]) {
+                    pos.x += 1;
+                    pos.y += 1;
+                    continue;
+                }
             }
-            if (!bitset.isSet(index(pos[0] - 1, pos[1] + 1))) {
-                pos[0] -= 1;
-                pos[1] += 1;
-                continue;
+            if (pos.y == 0) {
+                return count + 1;
             }
-            if (!bitset.isSet(index(pos[0] + 1, pos[1] + 1))) {
-                pos[0] += 1;
-                pos[1] += 1;
-                continue;
-            }
-            bitset.set(index(pos[0], pos[1]));
+            bitset[index(pos.x, pos.y)] = true;
             count += 1;
             continue :outer;
         }
-        break;
+        return count;
     }
-
-    return .{ count, 0 };
 }
 
-fn index(x: u8, y: u8) usize {
-    return @as(usize, @intCast(x)) + @as(usize, @intCast(y)) * 256;
+fn index(x: u16, y: u8) usize {
+    return @as(usize, @intCast(x)) * 256 + @as(usize, @intCast(y));
 }
 
 test {
@@ -83,5 +100,5 @@ test {
     ;
     const results = try solve(std.testing.allocator, input);
     try std.testing.expectEqual(24, results[0]);
-    // try std.testing.expectEqual(93, results[1]);
+    try std.testing.expectEqual(93, results[1]);
 }
